de66bc90b3c42c8b923ca28e3f82a61deb8727c2
david.monz <david.monz@macio.de>
2022-01-16 19:42:46 +0100

    Add sample code

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './assets/styles';
 
-import App from './views/app';
+import { App } from './views/app';
 
 const rootElement = document.querySelector('#root');
 ReactDOM.render(<App />, rootElement);
--- src/views/app/index.tsx
+++ src/views/app/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import './style.css';
 
-export default function App() {
-    return <div>Hello World!</div>;
-}
+export const App: FunctionComponent = () => <div className="app">Hello World!</div>;
